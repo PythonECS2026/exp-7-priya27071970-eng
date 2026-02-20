de96fc122f801ff0cd05edf992db74155ae88b52
priya27071970-eng <priya27071970@gmail.com>
2026-02-20 20:32:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -40,14 +40,3 @@ if delete_uin == "251S055":
     )
 print(f"Final Student Records: {students}")
 # Write your code here
-# TODO: Add a new Student Record 
-
-# TODO: Update the Grade of Student of UIN 251P055
-
-# TODO: Remove Student with UIN 251P026
-
-
-# Stop coding here
-print(f"Final Student Records: {students_records}")
-
-
